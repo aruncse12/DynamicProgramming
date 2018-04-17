1c95a286b3db15702e591f8db110829fa13bbb89
Arun Prasath J <aruncse12@gmail.com>
2018-04-18 00:23:14 +0530

    Fib

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
-
+#define MAX 100
+int fib[MAX];
+void init()
+{
+    int i;
+    for(i=0;i<MAX;i++)
+        fib[i]=0;
+}
+int calc_fib(int num)
+{
+    if(fib[num]==0)
+    {
+        if(num <= 1)
+            return num;
+        else
+        {
+            fib[num] = calc_fib(num-1)+calc_fib(num-2);
+            return fib[num];
+        }
+            
+    }
+    else
+        return fib[num]; 
+}
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	return 0;
+	int num;
+    scanf("%d",&num);
+    init();
+    printf("%d",calc_fib(num));
 }
